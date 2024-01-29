403e0ec5025d844601d8dd52684e59ecb37832f3
Sif <sifemern@gmail.com>
2024-01-29 17:11:30 +0100

    Add redux toolkit

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,11 +1,15 @@
 import  { Link, useNavigate } from 'react-router-dom'
 import {Alert, Button, Label, Spinner, TextInput} from 'flowbite-react'
 import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/UserSlice'
 
 const SignIn = () => {
   const [formData, setFormData] = useState({})
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [loading, setLoading] = useState(false)
+  // const [errorMessage, setErrorMessage] = useState(null)
+  // const [loading, setLoading] = useState(false)
+  const {loading, error: errorMessage} = useSelector(state => state.user)
+  const dispatch = useDispatch()
   const navigate = useNavigate()
   const handleChange = (e) =>{
     setFormData({...formData, [e.target.id]: e.target.value.trim()})
@@ -13,11 +17,13 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     if(!formData.email || !formData.password ){
-      return setErrorMessage('Please fill out all fields.')
+      // return setErrorMessage('Please fill out all fields.')
+      return dispatch(signInFailure('Please fill all fields'))
     }
     try {
-      setLoading(true)
-      setErrorMessage(null)
+      // setLoading(true)
+      // setErrorMessage(null)
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -25,15 +31,18 @@ const SignIn = () => {
       });
       const data = await res.json();
       if (data.success === false){
-        return setErrorMessage(data.message)
+        // return setErrorMessage(data.message)
+        return dispatch(signInFailure(data.message))
       }
-      setLoading(false)
+      // setLoading(false)
       if(res.ok){
+        dispatch(signInSuccess(data))
         navigate('/')
       }
     } catch (error) {
-      setErrorMessage(error.message)
-      setLoading(false)
+      // setErrorMessage(error.message)
+      // setLoading(false)
+      dispatch(signInFailure(error.message))
     }
   }
 
