d6729d3469e99f9f029afaa92e5f776fa4d42d5b
Sif <sifemern@gmail.com>
2024-01-31 01:59:37 +0100

    complete dashboard sidebar

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -3,3 +3,5 @@ export { default as Footer } from "./Footer";
 export { default as OAuth } from "./OAuth";
 export { default as ThemeProvider } from "./ThemeProvider";
 export { default as PrivateRoute } from "./PrivateRoute";
+export { default as DashSidebar } from "./DashSidebar";
+export { default as DashProfile } from "./DashProfile";
